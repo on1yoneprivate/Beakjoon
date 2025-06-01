ee9cde4f4a805fc945bc33da3d0c69d36a260662
on1yoneprivate <144034703+on1yoneprivate@users.noreply.github.com>
2025-06-01 17:24:30 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32544 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,20 +1,17 @@
 import sys
-import math
 
 input = sys.stdin.readline
 
 l = int(input())
-
-a = input().rstrip()
-arr = list(a)
-num_arr = []
-
-for i in arr:
-    ans = ord(i) - 96       # 소문자 알파벳 a 아스키 코드 : 97
-    num_arr.append(ans)
+a = input().strip()
+mod = 1234567891
 
 result = 0
+power = 1
+
 for i in range(l):
-    result += num_arr[i] * 31**i
+    char = ord(a[i]) - 96       # 소문자 알파벳 a 아스키 코드 : 97
+    result = (result + char * power) % mod
+    power = (power * 31) % mod      # 지수 연산 누적 계산
 
 print(result)
